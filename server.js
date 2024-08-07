38b75950d5d67a9dcca9d129ab7ada1ffe30b43f
Bhaskar magapu <151642780+bhaskar16-github@users.noreply.github.com>
2024-08-07 15:57:35 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,39 @@
+const express= require("express");
+const app= express();  //instalisation
+app.use(express.json()); //json can format data transfer
+const port =8081;
+const toDoList=["Bhaskar","Magapu"];
+app.get("/todos",(req,res)=>{
+    res.status(200).send(toDoList);
+});
+app.post("/todos",(req,res)=>{
+    let newToDoItem= req.body.item
+    toDoList.push(newToDoItem);
+    res.status(201).send({
+        message: "The todo got added sucessfully"
+    });
+});
+app.delete("/todos",(req,res)=>{
+    const itemToDelete= req.body.item
+    toDoList.find((element,index)=>{
+        if(element=== itemToDelete){
+            toDoList.splice(index,1);
+        }
+    });
+     res.status(202).send({
+        message: "Deleted item"
+});
+});
+app.all("/todos",(req,res)=>{
+    res.status(501).send();
+});
+app.all("*",(req,res)=>{
+    res.status(404).send();
+});
+
+app.listen(port,()=>{
+    console.log (`Express Js${port}`);
+});
+
+
+
